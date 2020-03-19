c7009f3feeb252edaf61008215eb562ee0f2e5db
AlexTect.Tips <alextechtips888@gmail.com>
2020-03-19 22:42:22 +0800

    add header with nav

--- includes/header.php
+++ includes/header.php
@@ -8,4 +8,13 @@
     <title>HTML 5 template</title>
 </head>
 <body>
+    <header>
+        <div class="logo"><img src="./img/startup_200.png" alt="site logo"></div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about.php">About</a></li>
+            </ul>
+        </nav>
+    </header>
     <h1 class="title"><?php echo($page_title); ?></h1>
