9180a6e50c6b87fa2d448ce4df49488d13b05dbe
AlexTect.Tips <alextechtips888@gmail.com>
2020-03-19 12:57:08 +0800

    move files to structure

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<footer>
+    <footer>
         <div class="footer_head">Copyright 2020 © All rights Reserved. Design by Comp-SQ Studio</div>
     </footer>
 </body>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php include_once("header.php"); ?>
+<?php include_once("./includes/header.php"); ?>
 <body>
     <h1 class="title">hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quo atque itaque cupiditate! Velit quasi hic facere officiis, esse consequuntur et aperiam libero non aut assumenda earum recusandae quam unde.</p>
+    <p>This is the last line</p>
 
-
-<?php include_once("footer.php");
+<?php include_once("./includes/footer.php");
