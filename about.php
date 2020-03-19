9fce11591c9fcc081e22242682837409fa330f30
AlexTect.Tips <alextechtips888@gmail.com>
2020-03-19 12:59:42 +0800

    add about page

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include_once("./includes/header.php"); ?>
+<body>
+    <h1 class="title">About Us</h1>
+    <p>All about us</p>
+
+<?php include_once("./includes/footer.php");
--- index.php
+++ index.php
@@ -3,5 +3,6 @@
     <h1 class="title">hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quo atque itaque cupiditate! Velit quasi hic facere officiis, esse consequuntur et aperiam libero non aut assumenda earum recusandae quam unde.</p>
     <p>This is the last line</p>
+    <a href="./about.php">Go to About</a>
 
 <?php include_once("./includes/footer.php");
